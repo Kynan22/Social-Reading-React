b0a6403b03a484b96c7ed5f23bf28fd9a18ebb59
Kynan22 <31174798+Kynan22@users.noreply.github.com>
2020-07-15 03:37:14 +1000

    start home components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
  
 import { withAuthorization } from '../Session';
+
+import Reading from './reading';
  
 const HomePage = () => (
   <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
+    <Reading/>
   </div>
 );
  
---
+++ src/components/Home/reading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Firebase from '../Firebase/firebase';
+
+const Reading = () => {
+    
+    return(
+        <div>
+            <h2>Reading</h2>
+            <img src="animal_farm.jpg"></img>
+            
+        </div>
+    );
+}
+
+export default Reading;
