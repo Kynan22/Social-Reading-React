bd45ed7ced66a7a1f1073efb0329971b0eea8e44
Kynan22 <31174798+Kynan22@users.noreply.github.com>
2020-07-16 00:05:21 +1000

    document.js update

--- src/components/Firebase/document.js
+++ src/components/Firebase/document.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import firebase from 'firebase';
 import { useDocument } from 'react-firebase-hooks/firestore';
-
-import Book from '../Classes/book';
-
-const FirestoreDocument = () => {
+const FirestoreDocument = (userid) => {
   const [value, loading, error] = useDocument(
-    firebase.firestore().doc('users/7Y4WxQ7H0DefZk3grcR8nOn7oV52/reading/9780316032834'),
+    firebase.firestore().doc('users/s/reading/9780316032834'),
     {
       snapshotListenOptions: { includeMetadataChanges: true },
     }
@@ -14,6 +11,7 @@ const FirestoreDocument = () => {
   return (
     <div>
       <p>
+        
         {error && <strong>Error: {JSON.stringify(error)}</strong>}
         {loading && <span>Document: Loading...</span>}
         {value && <span>Document: {JSON.stringify(value.data())}</span>}
